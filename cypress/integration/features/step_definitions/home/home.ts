c6d2d4d2cb084c76aeb38cbd329d33814d03db35
prashanth-sams <sams.prashanth@gmail.com>
2020-11-01 01:33:25 +0400

    first commit

--- cypress/integration/features/step_definitions/home/home.ts
+++ cypress/integration/features/step_definitions/home/home.ts
@@ -1,9 +1,9 @@
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Given, When, Then } from "cypress-cucumber-preprocessor/steps";
 
-Given('I am on the homepage', () => {
-  cy.visit('https://www.google.com/')
+Given("I am on the homepage", () => {
+  cy.visit("https://www.google.com/");
 });
 
-Then(`I see {string} in the title`, title => {
-  cy.title().should('include', title);
+Then(`I see {string} in the title`, (title) => {
+  cy.title().should("include", title);
 });
--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -2,7 +2,7 @@
  * @type {Cypress.PluginConfig}
  */
 
-const webpackPreprocessor = require('@cypress/webpack-preprocessor')
+const webpackPreprocessor = require("@cypress/webpack-preprocessor");
 
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
@@ -10,14 +10,14 @@ module.exports = (on, config) => {
 
   const options = {
     webpackOptions: require("../webpack.config"),
-    watchOptions: {}
+    watchOptions: {},
   };
 
   on("file:preprocessor", webpackPreprocessor(options));
 
-  on('before:browser:launch', (browser, launchOptions) => {
-    launchOptions.args.push('--proxy-bypass-list=<-loopback>')
-    
-    return launchOptions
+  on("before:browser:launch", (browser, launchOptions) => {
+    launchOptions.args.push("--proxy-bypass-list=<-loopback>");
+
+    return launchOptions;
   });
-}
+};
