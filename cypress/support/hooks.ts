e0901b74062d361f13c25823ef8f61fabda71864
prashanth-sams <sams.prashanth@gmail.com>
2020-11-02 14:52:47 +0400

    hooks and xml report

---
+++ cypress/support/hooks.ts
@@ -0,0 +1,18 @@
+import { Before, After } from "cypress-cucumber-preprocessor/steps";
+
+beforeEach(() => {
+    cy.clearLocalStorage();
+    cy.clearCookies();
+});
+  
+Before({ tags: "@home" }, () => {
+    cy.log('before @home scenario')
+});
+
+After({ tags: "@home" }, () => {
+    cy.log('after @home scenario')
+});
+
+afterEach(() => {
+    cy.log('after each hook')
+});
